80ad48bea818f180af1757cb72aa224eda9d5d79
Serik Kumarbek <s.kumarbek@mail.ru>
2025-09-07 18:15:33 +0500

    поменял бейс,мейн,и ордер пейдж

--- web_pages/base_page.py
+++ web_pages/base_page.py
@@ -3,57 +3,46 @@ from selenium.webdriver import ActionChains
 from seletools.actions import drag_and_drop
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class BasePage:
     def __init__(self, driver):
+        """Базовый класс страницы."""
         self.driver = driver
         self.wait = WebDriverWait(driver, 15)
 
-    def click_on_element(self, locator):
-        """Кликаем по элементу"""
-        self.driver.find_element(*locator).click()
-
-    def wait_until_element_visibility(self, locator):
-        """Дождаться видимости элемента"""
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
-
-    def wait_for_element_to_be_clickable(self, locator):
-        """Ожидание, что элемент станет кликабельным"""
-        WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable(locator))
-
-    def get_actually_text(self, locator):
-        """Получить текущий текст"""
-        return self.driver.find_element(*locator).text
-
-    def get_current_url(self):
-        """Получить текущую ссылку"""
-        return self.driver.current_url
-
-    def check_invisibility(self, locator) -> object:
-        """Проверить что элемент невидим"""
-        return WebDriverWait(self.driver, 10).until(EC.invisibility_of_element(locator))
-
-    def check_presence(self, locator):
-        """Проверить присутствие элемента на странице"""
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
-        return self.driver.find_element(*locator)
-
-    def drag_and_drop_on_element(self, locator_one, locator_two):
-        """Перетащить элемент"""
-        draggable = self.driver.find_element(*locator_one)
-        droppable = self.driver.find_element(*locator_two)
-        drag_and_drop(self.driver, draggable, droppable)
-
-    def move_to_element_and_click(self, locator):
-        """Переместиться до элемента и кликнуть"""
-        element = self.driver.find_element(*locator)
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element).click().perform()
-
     def find(self, locator):
-        """Ожидание элемента и возвращение его"""
+        """Найти элемент (ждём его появления в DOM)."""
         return self.wait.until(EC.presence_of_element_located(locator))
 
     def click(self, locator):
-        """Ожидание кликабельного элемента и клик"""
+        """Клик по элементу (ждём кликабельности)."""
         element = self.wait.until(EC.element_to_be_clickable(locator))
         element.click()
+
+    def get_text(self, locator, strip=True):
+        """Получить текст элемента."""
+        element = self.find(locator)
+        return element.text.strip() if strip else element.text
+
+    def get_current_url(self):
+        """Текущий URL."""
+        return self.driver.current_url
+
+    def wait_until_element_visibility(self, locator):
+        """Ждать, пока элемент станет видимым."""
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def wait_for_invisibility(self, locator):
+        """Ждать, пока элемент исчезнет/станет невидимым."""
+        return self.wait.until(EC.invisibility_of_element_located(locator))
+
+    def drag_and_drop_on_element(self, locator_one, locator_two):
+        """Перетащить элемент на другой элемент."""
+        draggable = self.find(locator_one)
+        droppable = self.find(locator_two)
+        drag_and_drop(self.driver, draggable, droppable)
+
+    def move_to_element_and_click(self, locator):
+        """Навести курсор и кликнуть."""
+        element = self.find(locator)
+        ActionChains(self.driver).move_to_element(element).click().perform()
--- web_pages/order_feed_page.py
+++ web_pages/order_feed_page.py
@@ -1,51 +1,43 @@
-from selenium.webdriver.support import expected_conditions as EC
 from web_pages.base_page import BasePage
 from web_locators.locators import LoginPageLocators, MainPageLocators, OrderFeedLocators
-from seletools.actions import drag_and_drop
 
 
 class OrderFeedPage(BasePage):
+
     def login_user(self, email, password, login_url):
         """Логин пользователя"""
         self.driver.get(login_url)
-        self.wait.until(EC.presence_of_element_located(LoginPageLocators.EMAIL_INPUT)).send_keys(email)
-        self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
-        self.wait.until(EC.element_to_be_clickable(LoginPageLocators.LOGIN_BTN)).click()
-        self.wait.until(EC.presence_of_element_located(MainPageLocators.MAIN_TITLE))
+        self.find(LoginPageLocators.EMAIL_INPUT).send_keys(email)
+        self.find(LoginPageLocators.PASSWORD_INPUT).send_keys(password)
+        self.click(LoginPageLocators.LOGIN_BTN)
+        self.find(MainPageLocators.MAIN_TITLE)
 
     def get_order_counter_today(self, order_feed_url):
-        """Получаем значение счётчика заказов 'Выполнено за сегодня'"""
+        """Значение счётчика заказов 'Выполнено за сегодня'"""
         self.driver.get(order_feed_url)
-        counter_element = self.wait.until(EC.presence_of_element_located(OrderFeedLocators.COUNTER_TODAY))
-        return int(counter_element.text)
+        return int(self.get_text(OrderFeedLocators.COUNTER_TODAY))
 
     def add_bun_to_constructor(self, constructor_url):
-        """Добавляем булку в конструктор"""
+        """Добавление булки в конструктор"""
         self.driver.get(constructor_url)
-        bun = self.wait.until(EC.presence_of_element_located(MainPageLocators.BUN_CARD))
-        drop_area = self.driver.find_element(*MainPageLocators.ORDER_BTN)
-        drag_and_drop(self.driver, bun, drop_area)
+        self.drag_and_drop_on_element(MainPageLocators.BUN_CARD, MainPageLocators.ORDER_BTN)
 
     def place_order(self):
-        """Оформляем заказ"""
-        order_btn_locator = MainPageLocators.ORDER_BTN
-        self.move_to_element_and_click(order_btn_locator)
+        """Оформление заказа"""
+        self.move_to_element_and_click(MainPageLocators.ORDER_BTN)
 
     def close_modal(self):
-        """Закрываем модальное окно"""
-        overlay_locator = OrderFeedLocators.OVERLAY
-        modal_close_locator = MainPageLocators.MODAL_CLOSE_BTN
-        self.wait.until(EC.invisibility_of_element_located(overlay_locator))
-        self.move_to_element_and_click(modal_close_locator)
+        """Закрытие модального окна"""
+        self.wait_for_invisibility(OrderFeedLocators.OVERLAY)
+        self.move_to_element_and_click(MainPageLocators.MODAL_CLOSE_BTN)
 
     def get_order_counter_total(self, order_feed_url):
-        """Получаем значение счётчика заказов 'Выполнено за все время'"""
+        """Значение счётчика заказов 'Выполнено за все время'"""
         self.driver.get(order_feed_url)
-        counter_element = self.wait.until(EC.presence_of_element_located(OrderFeedLocators.COUNTER_TOTAL))
-        return int(counter_element.text)
+        return int(self.get_text(OrderFeedLocators.COUNTER_TOTAL))
 
     def create_order(self, main_url):
-        """Добавляем булку в конструктор, оформляем заказ и закрываем модалку"""
+        """Создание заказа (булка → оформить → закрыть модалку)"""
         self.add_bun_to_constructor(main_url)
         self.place_order()
         self.close_modal()
